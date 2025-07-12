cc5b6249fa30fcbb61198b84d5d87ae0965393f0
Prateek Bhardwaj <prateek.bhardwaj@quizizz.com>
2025-07-12 18:27:31 +0530

    first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-white text-gray-800">
+      <img
+        src="https://superbryn.s3.us-east-1.amazonaws.com/Prateek+Bhardwaj+LinkedIn.jpegI"
+        alt="Photo"
+        className="w-32 h-32 rounded-full mb-4 shadow-md"
+      />
+      <h1 className="text-2xl font-semibold">Prateek Bhardwaj</h1>
+    </div>
+  );
+}
