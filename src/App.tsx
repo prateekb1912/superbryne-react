f2026459f7fada441f2f598903f18a7ce8473377
Prateek Bhardwaj <prateek.bhardwaj@quizizz.com>
2025-07-12 18:31:18 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ export default function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-white text-gray-800">
       <img
-        src="https://superbryn.s3.us-east-1.amazonaws.com/Prateek+Bhardwaj+LinkedIn.jpegI"
+        src="https://superbryn.s3.us-east-1.amazonaws.com/Prateek+Bhardwaj+LinkedIn.jpeg"
         alt="Photo"
         className="w-32 h-32 rounded-full mb-4 shadow-md"
       />
